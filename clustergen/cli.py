5a3c802ca98f5792dec19d5780cc72ebc8815738
Juan Lara <julara@unal.edu.co>
2022-05-23 20:40:52 -0500

    First version of the cli

--- clustergen/cli.py
+++ clustergen/cli.py
@@ -1,12 +1,36 @@
 from argparse import ArgumentParser, Namespace
 from clustergen.types import StrategyEnum, DatasetEnum, CliParams
 from clustergen.clustering import ModelProxy
+from clustergen.datasets import DatasetProxy
+from clustergen.training import Trainer
+from clustergen.visualization import ClusteringVisualization
 
 def make_parser() -> ArgumentParser:
     parser = ArgumentParser(description="Terminal utility to compare different clustering strategies.")
-    parser.add_argument("--strategy", type=str, required=True)
-    parser.add_argument("--n_clusters", type=int, default=2)
-    parser.add_argument("--dataset", type=str, default="blobs")
+    parser.add_argument(
+            "--strategy", type=str, required=True,
+            help="Clustering strategy to use. {'kmeans', 'gmm', 'spectral'}"
+            )
+    parser.add_argument(
+            "--n_clusters", type=int, default=2,
+            help="Number of clusters to find."
+            )
+    parser.add_argument(
+            "--n_samples", type=int, default=1000,
+            help="Number of samples to generate."
+            )
+    parser.add_argument(
+            "--noise", type=float, default=0.1,
+            help="Noise level."
+            )
+    parser.add_argument(
+            "--dataset", type=str, default="blobs",
+            help="Type of generated dataset. {'blobs', 'moons', 'circles'}"
+            )
+    parser.add_argument(
+            "--save_path", type=str, default="im.png",
+            help="Save path."
+            )
     return parser
 
 def validate_args(args: Namespace) -> CliParams:
@@ -21,7 +45,9 @@ def validate_args(args: Namespace) -> CliParams:
         raise Exception(f"<{args.dataset}> is not a valid DatasetEnum.")
 
     return CliParams(
-            strategy=strategy, dataset=dataset, n_clusters=args.n_clusters
+            strategy=strategy, dataset=dataset,
+            n_clusters=args.n_clusters, n_samples=args.n_samples,
+            noise=args.noise, save_path=args.save_path
             )
 
 def main():
@@ -29,6 +55,12 @@ def main():
     cliparams = validate_args(parser.parse_args())
 
     model_proxy = ModelProxy(cliparams.strategy, cliparams.n_clusters)
+    dataset_proxy = DatasetProxy(cliparams.dataset, cliparams.noise, cliparams.n_samples)
 
     model = model_proxy.resolve()
-    print(model)
+    dataset = dataset_proxy.resolve()
+
+    trainer = Trainer(model, dataset).train()
+
+    visualization = ClusteringVisualization(trainer).draw()
+    visualization.save(cliparams.save_path)
